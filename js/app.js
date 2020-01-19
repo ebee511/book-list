853b662f73787375cf057cf9f794d81a11da3292
Erica Bee <ebee511@gmail.com>
2020-01-19 13:04:01 -0700

    create book constructor and UI update func

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-console.log("connected");
+// Variables
+let submitBtn = document.getElementById("submitBtn");
+let bookTitle = document.getElementById("book-title");
+let bookAuthor = document.getElementById("author");
+let bookISBN = document.getElementById("isbn");
+let table = document.getElementById("table");
+
+// Create book constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// Add event listener for submit button
+submitBtn.addEventListener("click", function() {
+  let title = bookTitle.value;
+  let author = bookAuthor.value;
+  let isbn = bookISBN.value;
+
+  let book = new Book(title, author, isbn);
+  addBookToUI(title, author, isbn);
+});
+
+function addBookToUI(title, author, isbn) {
+  //create a new row
+  let row = document.createElement("tr");
+  //create table data
+  let td1 = document.createElement("td");
+  td1.textContent = title;
+  //create table data
+  let td2 = document.createElement("td");
+  td2.textContent = author;
+  //create table data
+  let td3 = document.createElement("td");
+  td3.textContent = isbn;
+  //append td's to tr
+  row.append(td1, td2, td3);
+  //table append row
+  table.append(row);
+}
